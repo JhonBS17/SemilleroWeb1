4fb74dccfdb1f47fe747561565b921f750e93d3c
JhonB <jhon.buesaquillo@correo.usa.edu.co>
2020-08-22 15:10:45 -0500

    3er commit

--- src/index.js
+++ src/index.js
@@ -19,7 +19,7 @@ const request = require('request');
 // const upload = multer({storage});
 
 app.use(express.json());
-app.use(express.urlencoded({extended: true}));
+app.use(express.urlencoded({ extended: true }));
 app.use(morgan('dev'));
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'ejs');
@@ -40,16 +40,62 @@ app.get('/', (req, res) => {
 //     });
 // });
 
-// app.post('/subir', async (req, res) => {
-//     const data = new Data(req.body);
-//     await data.save();
-//     res.send(data);
-// });
+app.post('/datos', (req, res) => {
+    // // var MQ2Value = req.query.mq2;
+    // // var voltaje = (MQ2Value*5)/1023;
+    // // var Rs = 1000*((5-voltaje)/voltaje); //RL=1k
+    // // var alcohol = 0.4091*Math.pow(Rs/5463, -1.497);
 
-// app.post('/subir', upload.single('file'), (req, res) => {
-//     console.log('Storage location');
-//     return res.send(req.file);
-// });
+    // const RL_VALUE = 5;      // Resistencia RL del modulo en Kilo ohms
+    // const R0 = 10;          // Resistencia R0 del sensor en Kilo ohms
+
+    // // Datos para lectura multiple
+    // const READ_SAMPLE_INTERVAL = 100;    // Tiempo entre muestras
+    // const READ_SAMPLE_TIMES = 5;       // Numero muestras
+
+    // // Ajustar estos valores para vuestro sensor según el Datasheet
+    // // (opcionalmente, según la calibración que hayáis realizado)
+    // const X0 = 200;
+    // const Y0 = 1.7;
+    // const X1 = 10000;
+    // const Y1 = 0.28;
+
+    // // Puntos de la curva de concentración {X, Y}
+    // const punto0 = [Math.log10(X0), Math.log10(Y0)];
+    // const punto1 = [Math.log10(X1), Math.log10(Y1)];
+
+    // // Calcular pendiente y coordenada abscisas
+    // const scope = (punto1[1] - punto0[1]) / (punto1[0] - punto0[0]);
+    // const coord = punto0[1] - punto0[0] * scope;
+
+    // var rs_med = readMQ(req.query.mq2);      // Obtener la Rs promedio
+    // var concentration = getConcentration(rs_med / R0);   // Obtener la concentración
+
+    console.log(req.query);
+
+    res.end();
+
+});
+
+// Obtener la resistencia promedio en N muestras
+function readMQ(mq_pin) {
+    const rs = 0;
+    for (var i = 0; i < READ_SAMPLE_TIMES; i++) {
+        rs += getMQResistance(analogRead(mq_pin));
+        delay(READ_SAMPLE_INTERVAL);
+    }
+    return rs / READ_SAMPLE_TIMES;
+}
+
+// Obtener resistencia a partir de la lectura analogica
+function getMQResistance(raw_adc){
+    return ((RL_VALUE / 1000.0 * (1023 - raw_adc) / raw_adc));
+}
+
+// Obtener concentracion 10^(coord + scope * log (rs/r0)
+function getConcentration(rs_ro_ratio){
+    return Math.pow(10, coord + scope * Math.log(rs_ro_ratio));
+}
 
 app.listen(PORT, () => {
     console.log('Server on port', PORT);
--- src/public/js/main.js
+++ src/public/js/main.js
@@ -5,52 +5,69 @@ $.ajax({
     url: "https://h4ks8u0iblvt8qn-sensores.adb.us-ashburn-1.oraclecloudapps.com/ords/sensores/data/datos",
     async: false,
     contentType: "application/json; charset=UTF-8",
-    success: function( result ){
+    success: function (result) {
         data = result.items;
     }
 });
 
-function search(){
+function search() {
     const fechaD = $("#Fdesde").val();
     const fechaH = $("#Fhasta").val();
-    if (fechaD == "" || fechaH == "" || $("#variable1 :selected").val() == 0){
+    if (fechaD == "" || fechaH == "" || $("#variable1 :selected").val() == 0) {
         Swal.fire(
             'Error!',
             'Se deben llenar todos los campos',
             'error'
         )
-    }else{
+    } else {
         $("#table1").empty();
         var tr1 = '<thead><tr><th>ID</th><th>Variable</th><th>Datos</th><th>Fecha</th><th>Hora</th>\
                 <th>Localización</th></tr></thead>';
         var cont = 0;
-        for(var i=0; i < data.length; i++) {
-            const fecha = data[i].fechainsercion.substring(0,10);
+        for (var i = 0; i < data.length; i++) {
+            const fecha = data[i].fechainsercion.substring(0, 10);
             if ($("#variable1 :selected").val() == data[i].tipodato && (fecha >= fechaD && fecha <= fechaH)) {
                 tr1 += '<tr id="tr2"><td>' + data[i].iddato + '</td>\
                 <td>' + data[i].tipodato + '</td>\
                 <td>' + data[i].dato + '</td>\
-                <td>' + data[i].fechainsercion.substring(0,10) + '</td>\
-                <td>' + data[i].fechainsercion.substring(11,19) + '</td>\
+                <td>' + data[i].fechainsercion.substring(0, 10) + '</td>\
+                <td>' + data[i].fechainsercion.substring(11, 19) + '</td>\
                 <td>' + data[i].localizacion + '</td></tr>';
                 cont += 1;
             }
         }
         $("#table1").append(tr1);
-        if (cont == 0){
+        if (cont == 0) {
             Swal.fire(
-            'Error!',
-            'No se han encotrado datos',
-            'error'
+                'Error!',
+                'No se han encotrado datos',
+                'error'
             )
-        }       
+        }
     }
 }
 
-function clean(){
-    $('#variable1').prop('selectedIndex',0);
+function clean() {
+    $('#variable1').prop('selectedIndex', 0);
     $("#Fdesde").val("");
     $("#Fhasta").val("");
 }
+
+$('#checkall').change(function () {
+    $('.cb-element').prop('checked', this.checked);
+});
+
+$('.cb-element').change(function () {
+    if ($('.cb-element:checked').length == $('.cb-element').length) {
+        $('#checkall').prop('checked', true);
+    }
+    else {
+        $('#checkall').prop('checked', false);
+    }
+});
+
+$( "#cancelarCheck" ).click(function() {
+    $('[type="checkbox"]').prop('checked', false);
+});
 
 $('.collapse').collapse();
